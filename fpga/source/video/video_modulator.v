14739b444aa49f18ea4c05e02ccf0329e760e309
Jeffrey <jeffrey@visual-trials.io>
2023-04-10 00:03:11 +0200

    refactored video modulator to use 1 less DSP and lessening the timing pressure for inputs of y_s, i_s and q_s flip flops

--- fpga/source/video/video_modulator.v
+++ fpga/source/video/video_modulator.v
@@ -6,6 +6,7 @@ module video_modulator(
     input  wire  [3:0] r,
     input  wire  [3:0] g,
     input  wire  [3:0] b,
+
     input  wire        color_burst,
     input  wire        active,
     input  wire        sync_n_in,
@@ -17,35 +18,105 @@ module video_modulator(
     parameter Y_G = 53; // 75; //  0.587
     parameter Y_B = 10; // 14; //  0.114
 
-    parameter I_R =  76; //  0.5959
-    parameter I_G = -35; // -0.2746
-    parameter I_B = -41; // -0.3213
+    parameter I_R = 76; //  0.5959
+    parameter I_G = 35; // -0.2746 (since this should be -35, *after* multiplication the result is negated)
+    parameter I_B = 41; // -0.3213 (since this should be -41, *after* multiplication the result is negated)
 
-    parameter Q_R =  27; //  0.2115
-    parameter Q_G = -66; // -0.5227
-    parameter Q_B =  40; //  0.3112
+    parameter Q_R = 27; //  0.2115
+    parameter Q_G = 66; // -0.5227 (since this should be -66, *after* multiplication the result is negated)
+    parameter Q_B = 40; //  0.3112
 
-    wire signed [4:0] r_s = color_burst ? 5'd9 : {1'b0, r};
-    wire signed [4:0] g_s = color_burst ? 5'd9 : {1'b0, g};
-    wire signed [4:0] b_s = color_burst ? 5'd0 : {1'b0, b};
+    // We set up two DSPs for 4 of the 9 multiplications
 
+    wire [15:0] y_g_16, i_g_16;
+    wire [15:0] y_r_16, i_r_16;
+    
+    // We use one DSP for two 8x8 unsigned multiplications
+    video_modulator_mult_u8xu8_pair video_modulator_mult_ig_yg (
+        .clk(clk),
+        
+        // i_g = I_G * g
+        .input_1a_8(I_G[7:0]),
+        .input_1b_8({4'b0000, g}),
+        .output_1_16(i_g_16),
+        
+        // y_g = Y_G * g
+        .input_2a_8(Y_G[7:0]),
+        .input_2b_8({4'b0000, g}),
+        .output_2_16(y_g_16)
+    );
+    
+    // We use another DSP for two 8x8 unsigned multiplications
+    video_modulator_mult_u8xu8_pair video_modulator_mult_yr_ir (
+        .clk(clk),
+        
+        // y_r = Y_R * r
+        .input_1a_8(Y_R[7:0]),
+        .input_1b_8({4'b0000, r}),
+        .output_1_16(y_r_16),
+        
+        // i_r = I_R * r
+        .input_2a_8(I_R[7:0]),
+        .input_2b_8({4'b0000, r}),
+        .output_2_16(i_r_16)
+    );
+    
+    // We need these five differently shifted values to replace the remaining multiplications by additions
+    
+    wire [4:0] g_times_2  = { g, 1'b0 };
+    wire [9:0] g_times_64 = { g, 6'b000000 };
+
+    wire [4:0] b_times_2  = { b, 1'b0 };
+    wire [6:0] b_times_8  = { b, 3'b000 };
+    wire [8:0] b_times_32 = { b, 5'b00000 };
+    
+    // We put together all the 9 multiplication results (all unsigned so far)
+
+    wire [11:0] y_r, y_g, y_b;
+    wire [11:0] i_r, i_g, i_b;
+    wire [11:0] q_r, q_g, q_b;
+    
+    assign y_r = y_r_16[11:0];           // y_r = Y_R * r
+    assign y_g = y_g_16[11:0];           // y_g = Y_G * g
+    assign y_b = b_times_8 + b_times_2;  // y_b = Y_B * b and since Y_B is 10 (8+2): y_b = 8*b + 2*b
+    
+    assign q_r = y_r;                    // q_r = Q_R * r and since Q_R is Y_R: q_r = y_r
+    assign q_g = g_times_64 + g_times_2; // q_g = Q_G * g and since Q_G is 66 (64+2): q_g = 64*g + 2*g
+    assign q_b = b_times_32 + b_times_8; // q_b = Q_B * b and since Q_B is 40 (32+8): q_b = 32*b + 8*b
+
+    assign i_r = i_r_16[11:0];           // i_r = I_R * r
+    assign i_g = i_g_16[11:0];           // i_g = I_G * g
+    assign i_b = q_b + b;                // i_b = I_B * b and since I_B is 41 (32+8+1): i_b = q_b + 1*b
+    
     reg signed [11:0] y_s;
     reg signed [11:0] i_s;
     reg signed [11:0] q_s;
-
+    
     always @(posedge clk) begin
-        y_s <= (sync_n_in == 0) ? 12'd0 : 12'd544;
-        i_s <= 0;
-        q_s <= 0;
-
-        if (active) begin
-            y_s <= (Y_R * r_s) + (Y_G * g_s) + (Y_B * b_s) + (128 + 512);
-        end
-
-        if (active || color_burst) begin
-            i_s <= (I_R * r_s) + (I_G * g_s) + (I_B * b_s);
-            q_s <= (Q_R * r_s) + (Q_G * g_s) + (Q_B * b_s);
-        end
+        
+        case ({active, color_burst})
+            2'b00: begin
+                y_s <= (sync_n_in == 0) ? 12'd0 : 12'd544;
+                i_s <= 0;
+                q_s <= 0;
+            end
+            2'b01: begin
+                y_s <= (sync_n_in == 0) ? 12'd0 : 12'd544;
+                i_s <= (I_R * 5'd9) - (I_G * 5'd9) - (I_B * 5'd0);
+                q_s <= (Q_R * 5'd9) - (Q_G * 5'd9) + (Q_B * 5'd0);
+            end
+            2'b10: begin
+                y_s <= y_r + y_g + y_b + (128 + 512);
+                i_s <= i_r - i_g - i_b;                 // Effectively negating i_g and i_b here
+                q_s <= q_r - q_g + q_b;                 // Effectively negating q_g here
+            end
+            2'b11: begin
+                y_s <= (Y_R * 5'd9) + (Y_G * 5'd9) + (Y_B * 5'd0) + (128 + 512);
+                i_s <= (I_R * 5'd9) - (I_G * 5'd9) - (I_B * 5'd0);
+                q_s <= (Q_R * 5'd9) - (Q_G * 5'd9) + (Q_B * 5'd0);
+            end
+        endcase
+        
     end
 
     // Color burst frequency: 315/88 MHz = 3579545 Hz
@@ -80,7 +151,7 @@ module video_modulator(
             lum <= 255;
         else
             lum <= y_s[10:3];
-
+            
         chroma_s <= (cosval_s * i8_s) + (sinval_s * q8_s);
     end
 
